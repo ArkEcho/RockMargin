33115470aecee92f833e3c497e48ea971e0658f2
neofuturelabs <154937697+neofuturelabs@users.noreply.github.com>
2023-12-26 21:04:49 -0800

    Adjust the default visual settings to look good in VS2022 Dark Mode. Adjust the order of settings in the code to match the order displayed in the options screen.

--- RockMargin/Options/OptionsDefinition.cs
+++ RockMargin/Options/OptionsDefinition.cs
@@ -3,136 +3,168 @@ using Microsoft.VisualStudio.Text.Editor;
 
 namespace RockMargin
 {
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class WidthOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 75; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.Width; } }
-	}
+    // DEFAULT SETTINGS HERE
+    // Below order adjusted to match displayed order in VS options screen
+    // Note: Colors are in ARGB order. Ex: 0xff010203 means A=255, R=1, G=2, B=3  (display in options screen is "R, G, B" when A=ff. If A!=ff, display is "A, R, G, B")
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class AltHighlightsOption : ViewOptionDefinition<bool>
-	{
-		public override bool Default { get { return false; } }
-		public override EditorOptionKey<bool> Key { get { return OptionsKeys.AltHighlights; } }
-	}
+    // --- Change margin
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class ChangeMarginEnabledOption : ViewOptionDefinition<bool>
+    {
+        public override bool Default { get { return true; } }
+        public override EditorOptionKey<bool> Key { get { return OptionsKeys.ChangeMarginEnabled; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class HighlightsEnabledOption : ViewOptionDefinition<bool>
-	{
-		public override bool Default { get { return true; } }
-		public override EditorOptionKey<bool> Key { get { return OptionsKeys.HighlightsEnabled; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class SavedChangeColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff6ce26c; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.SavedChangeColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class MarginColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xfff0f0f0; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.MarginColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class UnsavedChangeColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xffffee62; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.UnsavedChangeColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class ScrollColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0x00000000; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.ScrollColor; } }
-	}
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class ThumbColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0x400000ff; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.ThumbColor; } }
-	}
+    // --- General
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class EnhancedTextRenderingOption : ViewOptionDefinition<bool>
+    {
+        public override bool Default { get { return true; } }
+        public override EditorOptionKey<bool> Key { get { return OptionsKeys.EnhancedTextRendering; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class TextColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xff505050; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.TextColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class WidthOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 110; } } // original: 75
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.Width; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class CommentsColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xff00a000; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.CommentsColor; } }
-	}
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class HighlightColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xffff0000; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.HighlightColor; } }
-	}
+    // --- Marks margin
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class BookmarkMarkColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff00ffff; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.BookmarkMarkColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class BackgroundColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xffffffff; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.BackgroundColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class BreakpointMarkColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff800000; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.BreakpointMarkColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class TextMarkerBackgroundColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xffadd8e6; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.TextMarkerBackgroundColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class TracepointMarkColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xfff08080; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.TracepointMarkColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class TextMarkerForegroundColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xff00008b; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.TextMarkerForegroundColor; } }
-	}
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class SavedChangeColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xff6ce26c; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.SavedChangeColor; } }
-	}
+    // --- Scroll coloring
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class BackgroundColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff1e1e1e; } } // original: 0xffffffff
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.BackgroundColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class UnsavedChangeColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xffffee62; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.UnsavedChangeColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class CommentsColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff006e00; } } // original: 0xff00a000
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.CommentsColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class ChangeMarginEnabledOption : ViewOptionDefinition<bool>
-	{
-		public override bool Default { get { return true; } }
-		public override EditorOptionKey<bool> Key { get { return OptionsKeys.ChangeMarginEnabled; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class MarginColorOption : ViewOptionDefinition<uint> // "Scroll margin"
+    {
+        public override uint Default { get { return 0xff1e1e1e; } } // original: 0xfff0f0f0
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.MarginColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class EnhancedTextRenderingOption : ViewOptionDefinition<bool>
-	{
-		public override bool Default { get { return true; } }
-		public override EditorOptionKey<bool> Key { get { return OptionsKeys.EnhancedTextRendering; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class ThumbColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0x784664ff; } } // original: 0x400000ff
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.ThumbColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class BookmarkMarkColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xff00ffff; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.BookmarkMarkColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class TextColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff505050; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.TextColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class BreakpointMarkColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xff800000; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.BreakpointMarkColor; } }
-	}
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class HighlightColorOption : ViewOptionDefinition<uint> // "Text markers"
+    {
+        public override uint Default { get { return 0xffff0000; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.HighlightColor; } }
+    }
 
-	[Export(typeof(EditorOptionDefinition))]
-	public sealed class TracepointMarkColorOption : ViewOptionDefinition<uint>
-	{
-		public override uint Default { get { return 0xfff08080; } }
-		public override EditorOptionKey<uint> Key { get { return OptionsKeys.TracepointMarkColor; } }
-	}
+
+
+    // --- Text markers
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class AltHighlightsOption : ViewOptionDefinition<bool> // "Alt + Double click"
+    {
+        public override bool Default { get { return false; } }
+        public override EditorOptionKey<bool> Key { get { return OptionsKeys.AltHighlights; } }
+    }
+
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class HighlightsEnabledOption : ViewOptionDefinition<bool>
+    {
+        public override bool Default { get { return true; } }
+        public override EditorOptionKey<bool> Key { get { return OptionsKeys.HighlightsEnabled; } }
+    }
+
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class TextMarkerBackgroundColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff00468c; } } // original: 0xffadd8e6
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.TextMarkerBackgroundColor; } }
+    }
+
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class TextMarkerForegroundColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0xff00008b; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.TextMarkerForegroundColor; } }
+    }
+
+
+
+
+    // ????? unused ?????
+    [Export(typeof(EditorOptionDefinition))]
+    public sealed class ScrollColorOption : ViewOptionDefinition<uint>
+    {
+        public override uint Default { get { return 0x00000000; } }
+        public override EditorOptionKey<uint> Key { get { return OptionsKeys.ScrollColor; } }
+    }
+
+   
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
 }
